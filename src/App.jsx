b6c0f2a4d9073b110c08b7606e5ed79e3a9c42e0
JuanCM <juancampuzano2356@gmail.com>
2025-09-16 16:28:31 -0500

    Remove react import in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 import TrainingForm from "./components/TrainingForm";
 import TrainingList from "./components/TrainingList";
 import "./App.css";
